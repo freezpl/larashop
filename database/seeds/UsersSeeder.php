8d489dc244bc07c29b14b6457c6e0a161d431d27
freezpl <freezpl@gmail.com>
2020-01-04 13:54:26 +0200

    add roles

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -7,10 +7,26 @@ class UsersSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'pasha',
-            'email' => 'a@gmail.com',
-            'password' => bcrypt('12345'),
+        // DB::table('users')->insert([
+        //     'name' => 'pasha',
+        //     'email' => 'a@gmail.com',
+        //     'password' => bcrypt('12345'),
+        // ]);
+
+        // DB::table('users')->insert([
+        //     'name' => '0668526407',
+        //     'password' => bcrypt('12345'),
+        // ]);
+
+        // $roles = array(
+        //     array('name'=>'admin'),
+        //     array('name'=>'user'),
+        // );
+        // DB::table('roles')->insert($roles);
+
+        DB::table('user_role')->insert([
+            ['user_id'=>'1', 'role_id'=>'1'],
+            ['user_id'=>'2', 'role_id'=>'2'],
         ]);
     }
 }
