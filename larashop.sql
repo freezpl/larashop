f278265be4150093a74c47e678d025beca9bd3af
freezpl <freezpl@gmail.com>
2020-01-24 08:41:07 +0200

    fix routes

--- larashop.sql
+++ larashop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Час створення: Січ 20 2020 р., 08:52
+-- Час створення: Січ 24 2020 р., 09:40
 -- Версія сервера: 8.0.15
--- Версія PHP: 7.1.22
+-- Версія PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,9 +34,20 @@ CREATE TABLE `categories` (
   `description` text COLLATE utf8mb4_unicode_ci,
   `thumb` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `parent_id` bigint(20) DEFAULT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT '1'
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `parent_id` bigint(20) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп даних таблиці `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`, `thumb`, `image`, `active`, `slug`, `parent_id`) VALUES
+(13, 'NewCat', NULL, 'category_thumb/3U326e0iaHcNTZSdSuBnHIxWey31RwJakam5jOF0.png', NULL, 1, 'newcat', NULL),
+(14, 'NewCat2', NULL, 'category_thumb/wRvH2RDOjY72CntWLp5IDqMkoKRt5wody8b48JSL.png', NULL, 1, 'newcat2', 13),
+(15, 'NewCat3', NULL, 'category_thumb/RYeLjHt8zxeuuY0VKAbs4ewhMqFZramSP6XmV1af.png', NULL, 1, 'newcat3', 13),
+(16, 'NewCat4', NULL, 'category_thumb/oJGKlAWb1jd64bzihkexgyPmPTkKtO4bb4hEkkhg.png', NULL, 1, 'newcat4', NULL);
 
 -- --------------------------------------------------------
 
@@ -62,7 +73,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2020_01_04_114114_drop_role_from_roles_table', 1),
 (6, '2020_01_04_114250_add_role_to_roles_table', 1),
 (7, '2020_01_04_114721_create_user_role_table', 1),
-(8, '2020_01_18_081831_create_categories_table', 2);
+(8, '2020_01_18_081831_create_categories_table', 2),
+(9, '2020_01_23_195530_add_slug_to_categories_table', 3),
+(10, '2020_01_23_212224_add_fk_to_categories_table', 4),
+(11, '2020_01_23_213829_add_parent_fk_to_categories_table', 5);
 
 -- --------------------------------------------------------
 
@@ -149,7 +163,8 @@ INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `categories_name_unique` (`name`);
+  ADD UNIQUE KEY `categories_name_unique` (`name`),
+  ADD KEY `categories_parent_id_foreign` (`parent_id`);
 
 --
 -- Індекси таблиці `migrations`
@@ -191,13 +206,13 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT для таблиці `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT для таблиці `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT для таблиці `roles`
@@ -214,6 +229,12 @@ ALTER TABLE `users`
 --
 -- Обмеження зовнішнього ключа збережених таблиць
 --
+
+--
+-- Обмеження зовнішнього ключа таблиці `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_parent_id_foreign` FOREIGN KEY (`parent_id`) REFERENCES `categories` (`id`);
 
 --
 -- Обмеження зовнішнього ключа таблиці `user_role`
--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,13 @@ Route::post('auth/administrator_login', 'Auth\AdminController@login');
 ///TEST///
 Route::post('register', 'Auth\UserController@register');
 Route::post('login', 'Auth\UserController@login');
-Route::get('open', 'DataController@open');
 
-Route::post('dashboard/categories/add', 'Dashboard\CategoriesController@add');
-Route::get('dashboard/categories/', 'Dashboard\CategoriesController@all');
+Route::get('categories/all', 'Dashboard\CategoriesController@all');
 
 Route::group(['middleware' => ['jwt.verify', 'role:admin']],
-    function () {
-        Route::get('user', 'Auth\UserController@getAuthenticatedUser');
-        Route::get('closed', 'DataController@closed');
+function () {
+    Route::get('user', 'Auth\UserController@getAuthenticatedUser');
+    Route::post('dashboard/categories/add', 'Dashboard\CategoriesController@add');
 
 
 });
